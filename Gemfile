5e384fd060e1ce9804a807023f483c236bfed5eb
Your Name <you@example.com>
2014-04-13 02:50:40 +0000

    fix picture page

--- Gemfile
+++ Gemfile
@@ -33,8 +33,10 @@ group :doc do
 end
  gem "twitter-bootstrap-rails"
 gem 'simple_form'
+gem 'carrierwave'
 
- 
+
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
 	private
 
 	def post_params
-		params.require(:post).permit(:title, :description)
+		params.require(:post).permit(:title, :description, :image)
 	end
 
 end
